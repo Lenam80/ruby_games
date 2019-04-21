931136dbbd3e5b6a504e97983f663213fd927981
Angelina Montanez <angelinamontanez@Angelinas-MacBook-Air.local>
2019-04-21 13:56:47 -0400

    message

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,21 +4,32 @@ class RubyGames::CLI
   def call
     list_character
     menu
+    goodbye
   end
 
 def list_character
   puts "New Character:"
-  puts <<-DOC.gsub /^\s*/, ''
-  1. First look at Kitana -https://www.vg247.com/2019/04/13/mortal-kombat-11-kitana-trailer/
-  2. First look at - Elder God Cetrion - https://www.vg247.com/2019/04/03/mortal-kombat-11-cetrion-trailer/
- DOC
+
+  1. First look at Kitana  #//www.vg247.com/2019/04/13/mortal-kombat-11-kitana-trailer/
+  2. First look at Elder God Cetrion #//www.vg247.com/2019/04/03/mortal-kombat-11-cetrion-trailer/
+ 
   end
 
 
 def menu
-puts "Select the character you would like to know more about:"
+  input = nil
+  while input != "exit"
+    puts "Select the character you would like to know more about:"
+    input = gets.strip.downcase
+    case input
+    when "1"
+      puts "MK11 first look at kitana"
+    when "2"
+      puts "MK11 first look at elder god cetrion"
+    end
+  end
 
-  input = gets.strip
-  case input 
+  def goodbye
+    puts "Thank you for stopping by"
   end
 end
